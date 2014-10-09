ecba485f9abb3c34ed96821ee374e21ba4e816b8
EdBoone <elboone@vcu.edu>
2014-10-09 15:16:01 -0400

    Changes to my idiot code
    Here it is

--- KotDoubleForced1.py
+++ KotDoubleForced1.py
@@ -13,15 +13,15 @@ def KotDF1(par1,initial_cond,start_t,end_t,incr):
      #-time-grid-----------------------------------
      t  = np.linspace(start_t, end_t, num = incr)
      #differential-eq-system----------------------
-     def funct(t,x):
+     def funct(x,t):
         D1, si, mu1, mu2, y1, y2, k1, k2, epsilon1, T1 = par1
-        omega1 = 2*pi/T1
-        xp1 = D1*( si*(1 + epsilon1*sin(omega1*t))-x[0])-mu1*x[0]*x[1]/y1/(k1+x[0])
+        omega1 = 2*np.pi/T1
+        xp1 = D1*( si*(1 + epsilon1*np.sin(omega1*t))-x[0])-mu1*x[0]*x[1]/y1/(k1+x[0])
         xp2 = mu1*x[0]*x[1]/(k1+x[0]) - D1*x[1] - mu2*x[1]*x[2]/y2/(k2+x[1])
         xp3 = mu2*x[1]*x[2]/(k2+x[1]) - D1*x[2]
         return [xp1, xp2, xp3]
      #integrate------------------------------------
-     ds = integrate.odeint(funct, t, initial_cond)
+     ds = integrate.odeint(funct, initial_cond, t)
      return (t, ds[:,0], ds[:,1], ds[:,2])
 #=======================================================
 
