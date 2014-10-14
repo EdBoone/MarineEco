37707f674073ec9f8530b00f5c4af16b4a70f58f
EdBoone <elboone@vcu.edu>
2014-10-14 15:18:12 -0400

    Working on Data Assimilation picture example
    This is just to create a plot to illustrate how data assimilation may
    change the model parameters as we move through time and assimilate at
    different times.

--- KotDoubleForced1.py
+++ KotDoubleForced1.py
@@ -42,22 +42,64 @@ epsilon1 = 0.6
 T1 = 15
 
 # Create containers
-x1 = np.zeros(shape=(101))
-x2 = np.zeros(shape=(101))
-x3 = np.zeros(shape=(101))
-t1 = np.zeros(shape=(101))
+x1 = np.zeros(shape=(1001))
+x2 = np.zeros(shape=(1001))
+x3 = np.zeros(shape=(1001))
+t1 = np.zeros(shape=(1001))
 
 # Initial Conditions
 x0 = [0.25, 0.5, 0.5]
 
 # Pack the parameters together
 par1 = (D1, si, mu1, mu2, y1, y2, k1, k2, epsilon1, T1)
-t1[:], x1[:], x2[:], x3[:] = KotDF1( par1, x0, 0, 15., 101 )
+t1[:], x1[:], x2[:], x3[:] = KotDF1( par1, x0, 0, 45., 1001 )
  
 plt.figure()
-plt.plot(t1, x1,'-b', t1 , x2,'-r')
-plt.ylim( [0,350])
+plt.plot(t1, x1,'-b', t1 , x2,'-r', t1, x3, '-k')
+plt.ylim( [0,100])
 #plt.legend(('Foxes', 'Rabbits'),'upper center',ncol=2)
 title1 = 'It works???'
 plt.title(title1)
+ 
+ 
+D1 = 0.1        # Dilution rate 1/hour
+si = 115        # units of si are mg/l
+# maximum specific growth rate of prey and preditor (1/hour)
+mu1 = 0.51       # prey
+mu2 = 0.19       # preditor
+# yields
+y1 = 0.1        # yield of prey per unit mass of substrate
+y2 = 0.59        # biomass yield of predator per unit mass of prey
+# half-saturation (Michaelis-Menton)
+k1 = 8.1          # prey
+k2 = 9.1          # predator
+# Other variables
+epsilon1 = 0.7
+T1 = 15
+
+# Create containers
+x1a = np.zeros(shape=(1001))
+x2a = np.zeros(shape=(1001))
+x3a = np.zeros(shape=(1001))
+t1a = np.zeros(shape=(1001))
+
+# Initial Conditions
+x0 = [0.25, 0.5, 0.5]
+
+# Pack the parameters together
+par1 = (D1, si, mu1, mu2, y1, y2, k1, k2, epsilon1, T1)
+t1a[:], x1a[:], x2a[:], x3a[:] = KotDF1( par1, x0, 0, 45., 1001 )
+
+x1d = np.random.poisson(x1)
+
+  
+plt.figure()
+plt.scatter(t1, x1d)
+plt.plot(t1, x1, '-b', t1a , x1a, '-r' )
+plt.ylim( [0,100] )
+plt.xlim( [0,45] )
+#plt.legend(('Foxes', 'Rabbits'),'upper center',ncol=2)
+title1 = 'It works???'
+plt.title(title1)
+ 
  
